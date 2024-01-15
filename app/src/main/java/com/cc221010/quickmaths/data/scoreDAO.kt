6450b960022f8f87f8a1f78c6f82e476ee1a0af4
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-15 17:21:09 +0100

    Created scoreDAO

--- app/src/main/java/com/cc221010/quickmaths/data/scoreDAO.kt
+++ app/src/main/java/com/cc221010/quickmaths/data/scoreDAO.kt
@@ -1,4 +1,26 @@
 package com.cc221010.quickmaths.data
 
-class scoreDAO {
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.cc221010.quickmaths.data.models.score
+import kotlinx.coroutines.flow.Flow
+
+interface scoreDAO {
+    @Insert
+    suspend fun insertScore(score:score);
+
+    @Update
+    suspend fun updateScore(score:score);
+
+    @Delete
+    suspend fun deleteScore(score:score);
+
+    @Query("SELECT * FROM scores")
+    fun getScores():Flow<List<score>>
+
+    @Query("SELECT * FROM scores WHERE id = :id")
+    fun getScore(id:Int):Flow<score>;
+
 }
