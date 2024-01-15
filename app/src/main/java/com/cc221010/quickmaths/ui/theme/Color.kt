35baf43f884e2aaa3acbff3048134bb7d39afef8
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-15 15:29:26 +0100

    Add Colors

--- app/src/main/java/com/cc221010/quickmaths/ui/theme/Color.kt
+++ app/src/main/java/com/cc221010/quickmaths/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.cc221010.quickmaths.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val primary = Color(0xFFD9ED92);
+val secondary = Color(0xFFB5E48C);
+val onPrimary = Color(0xFF34A0A4);
+val background = Color(0xFF1A759F);
+val shadow = Color(0xFF184E77);
--- app/src/main/java/com/cc221010/quickmaths/ui/theme/Theme.kt
+++ app/src/main/java/com/cc221010/quickmaths/ui/theme/Theme.kt
@@ -16,32 +16,22 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = primary,
+    secondary = secondary,
+    tertiary = shadow,
+    onPrimary = onPrimary,
+    background = background,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun QuickMathsTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme:Boolean = true,
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor:Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
