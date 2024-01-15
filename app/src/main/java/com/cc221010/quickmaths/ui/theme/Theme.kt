32bfe8c3882941210d86e87579c768d73bc58a18
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-15 16:01:32 +0100

    Remove Top Bar

--- app/src/main/java/com/cc221010/quickmaths/ui/theme/Theme.kt
+++ app/src/main/java/com/cc221010/quickmaths/ui/theme/Theme.kt
@@ -47,7 +47,7 @@ fun QuickMathsTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = colorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
