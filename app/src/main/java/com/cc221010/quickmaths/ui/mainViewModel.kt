ac9dd1045f8f949e3b0df99ec61719201c001212
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-15 14:26:01 +0100

    Added viewState to viewModel

--- app/src/main/java/com/cc221010/quickmaths/ui/mainViewModel.kt
+++ app/src/main/java/com/cc221010/quickmaths/ui/mainViewModel.kt
@@ -1,4 +1,11 @@
 package com.cc221010.quickmaths.ui
 
-class mainViewModel {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class mainViewModel(): ViewModel() {
+    private val _mainViewState = MutableStateFlow(mainViewState());
+    val mainViewState: StateFlow<mainViewState> = _mainViewState.asStateFlow();
 }
