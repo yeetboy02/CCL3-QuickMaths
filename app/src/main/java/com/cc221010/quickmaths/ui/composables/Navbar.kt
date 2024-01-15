92f883782cb39e4e6ceb557b85cffe9c3b77763c
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-15 16:58:37 +0100

    Styled Navbar

--- app/src/main/java/com/cc221010/quickmaths/ui/composables/Navbar.kt
+++ app/src/main/java/com/cc221010/quickmaths/ui/composables/Navbar.kt
@@ -1,19 +1,31 @@
 package com.cc221010.quickmaths.ui.composables
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -51,16 +63,25 @@ fun NavbarView(mainViewModel: mainViewModel) {
 
 @Composable
 fun BottomNavigationBar(navController:NavHostController, selectedScreen:Screen) {
-    BottomNavigation () {
-        NavigationBarItem(
-            selected = (selectedScreen == Screen.Home),
-            onClick = { navController.navigate(Screen.Home.route) },
-            icon = { Icon(painter = painterResource(id = R.drawable.home), contentDescription = "") })
-
-        NavigationBarItem(
-            selected = (selectedScreen == Screen.Highscores),
-            onClick = { navController.navigate(Screen.Highscores.route) },
-            icon = { Icon(painter = painterResource(id = R.drawable.highscore), contentDescription = "") })
-
+    Box(
+        modifier = Modifier.background(MaterialTheme.colorScheme.tertiary)
+    ) {
+        BottomNavigation (
+            modifier = Modifier.height(80.dp).padding(top = 4.dp),
+            backgroundColor = MaterialTheme.colorScheme.primary
+        ) {
+            NavigationBarItem(
+                selected = (selectedScreen == Screen.Home),
+                onClick = { navController.navigate(Screen.Home.route) },
+                icon = { Icon(painter = painterResource(id = R.drawable.home), contentDescription = "", modifier = Modifier.width(45.dp).height(45.dp), tint = MaterialTheme.colorScheme.onPrimary) },
+                colors = NavigationBarItemDefaults.colors(indicatorColor = MaterialTheme.colorScheme.background),
+            )
+            NavigationBarItem(
+                selected = (selectedScreen == Screen.Highscores),
+                onClick = { navController.navigate(Screen.Highscores.route) },
+                icon = { Icon(painter = painterResource(id = R.drawable.highscore), contentDescription = "", modifier = Modifier.width(45.dp).height(45.dp), tint = MaterialTheme.colorScheme.onPrimary) },
+                colors = NavigationBarItemDefaults.colors(indicatorColor = MaterialTheme.colorScheme.background),
+            )
+        }
     }
 }
