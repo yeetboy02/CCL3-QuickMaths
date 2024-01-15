def224bc9aca7cfaa154eccc62ac876e49888608
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-15 14:26:37 +0100

    Added selectScreen method

--- app/src/main/java/com/cc221010/quickmaths/ui/mainViewModel.kt
+++ app/src/main/java/com/cc221010/quickmaths/ui/mainViewModel.kt
@@ -1,11 +1,17 @@
 package com.cc221010.quickmaths.ui
 
 import androidx.lifecycle.ViewModel
+import com.cc221010.quickmaths.ui.composables.Screen
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class mainViewModel(): ViewModel() {
     private val _mainViewState = MutableStateFlow(mainViewState());
     val mainViewState: StateFlow<mainViewState> = _mainViewState.asStateFlow();
+
+    fun selectScreen(screen: Screen){
+        _mainViewState.update { it.copy(selectedScreen = screen) }
+    }
 }
