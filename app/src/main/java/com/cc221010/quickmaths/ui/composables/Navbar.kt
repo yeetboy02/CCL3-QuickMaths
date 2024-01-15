c44e2b82dc630d6eb258072962c67a9d59fdfcf6
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-15 15:14:58 +0100

    Added Navbar Icons

--- app/src/main/java/com/cc221010/quickmaths/ui/composables/Navbar.kt
+++ app/src/main/java/com/cc221010/quickmaths/ui/composables/Navbar.kt
@@ -6,16 +6,19 @@ import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.cc221010.quickmaths.R
 import com.cc221010.quickmaths.ui.mainViewModel
 
 sealed class Screen(val route: String){
@@ -52,12 +55,12 @@ fun BottomNavigationBar(navController:NavHostController, selectedScreen:Screen) 
         NavigationBarItem(
             selected = (selectedScreen == Screen.Home),
             onClick = { navController.navigate(Screen.Home.route) },
-            icon = { Icon(imageVector = Icons.Default.Home, contentDescription = "") })
+            icon = { Icon(painter = painterResource(id = R.drawable.home), contentDescription = "") })
 
         NavigationBarItem(
             selected = (selectedScreen == Screen.Highscores),
             onClick = { navController.navigate(Screen.Highscores.route) },
-            icon = { Icon(imageVector = Icons.Default.Star, contentDescription = "") })
+            icon = { Icon(painter = painterResource(id = R.drawable.highscore), contentDescription = "") })
 
     }
 }
