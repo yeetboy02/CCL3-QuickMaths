b941aaea828e80f70c43853bbe70cf5414e7685d
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-15 17:17:13 +0100

    Created ScoreDatabase

--- app/src/main/java/com/cc221010/quickmaths/data/scoreDatabase.kt
+++ app/src/main/java/com/cc221010/quickmaths/data/scoreDatabase.kt
@@ -1,4 +1,10 @@
 package com.cc221010.quickmaths.data
 
-class scoreDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.cc221010.quickmaths.data.models.score
+
+@Database(entities = [score::class], version = 1)
+abstract class scoreDatabase:RoomDatabase() {
+    abstract val dao:scoreDAO;
 }
