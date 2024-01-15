5e284059fd81e98e3c397d3bfa548345ab3f2621
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-15 17:09:14 +0100

    Created Score Model

--- app/src/main/java/com/cc221010/quickmaths/data/models/score.kt
+++ app/src/main/java/com/cc221010/quickmaths/data/models/score.kt
@@ -1,4 +1,13 @@
 package com.cc221010.quickmaths.data.models
 
-class score {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(tableName = "scores")
+data class score (
+    val name:String,
+    val points:Int,
+    val date: Date,
+    @PrimaryKey(autoGenerate = true) val id:Int = 0,
+)
