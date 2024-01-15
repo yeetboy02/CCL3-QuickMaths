d9fa71b5065f4d24fb6e549220b25d8f0135c05c
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-15 14:33:23 +0100

    Added viewModel to MainActivity

--- app/src/main/java/com/cc221010/quickmaths/MainActivity.kt
+++ app/src/main/java/com/cc221010/quickmaths/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cc221010.quickmaths
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,9 +11,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.cc221010.quickmaths.ui.mainViewModel
 import com.cc221010.quickmaths.ui.theme.QuickMathsTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val mainViewModel by viewModels<mainViewModel>(
+        factoryProducer = {
+            object:ViewModelProvider.Factory{
+                override fun <T:ViewModel> create(modelClass:Class<T>):T{
+                    return mainViewModel() as T;
+                }
+            }
+        }
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
