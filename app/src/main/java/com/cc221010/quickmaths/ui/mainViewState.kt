27a996527697b8ce17ece4a7520c7e85f0f148ba
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-17 10:05:29 +0100

    Added List of Scores to viewState

--- app/src/main/java/com/cc221010/quickmaths/ui/mainViewState.kt
+++ app/src/main/java/com/cc221010/quickmaths/ui/mainViewState.kt
@@ -1,7 +1,9 @@
 package com.cc221010.quickmaths.ui
 
+import com.cc221010.quickmaths.data.models.score
 import com.cc221010.quickmaths.ui.composables.Screen
 
 data class mainViewState(
+    val scores:List<score> = emptyList(),
     val selectedScreen: Screen = Screen.Home,
 )
