5698c76b9494d106f778ad0d3f7738a8bd3886ad
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-17 12:50:36 +0100

    Added currEditScore to State

--- app/src/main/java/com/cc221010/quickmaths/ui/mainViewState.kt
+++ app/src/main/java/com/cc221010/quickmaths/ui/mainViewState.kt
@@ -4,6 +4,7 @@ import com.cc221010.quickmaths.data.models.score
 import com.cc221010.quickmaths.ui.composables.Screen
 
 data class mainViewState(
+    val currEditScore:score? = null,
     val lastScore:score? = null,
     val scores:List<score> = emptyList(),
     val selectedScreen: Screen = Screen.Home,
