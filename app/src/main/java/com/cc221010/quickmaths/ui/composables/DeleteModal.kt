d3fdcfc3cfcf4901f9afd31412024fdac4e566a5
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-17 13:45:05 +0100

    Created rudimentary DeleteModal

--- app/src/main/java/com/cc221010/quickmaths/ui/composables/DeleteModal.kt
+++ app/src/main/java/com/cc221010/quickmaths/ui/composables/DeleteModal.kt
@@ -1,2 +1,18 @@
 package com.cc221010.quickmaths.ui.composables
 
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import com.cc221010.quickmaths.ui.mainViewModel
+
+@Composable
+fun deleteModal(mainViewModel:mainViewModel) {
+    val state = mainViewModel.mainViewState.collectAsState();
+    if (state.value.deleteModalOpen) {
+        AlertDialog(
+            onDismissRequest = { mainViewModel.closeDeleteModal(false) },
+            text = { Text(text = state.value.currEditScore!!.id.toString()) },
+            buttons = { /*TODO*/ })
+    }
+}
