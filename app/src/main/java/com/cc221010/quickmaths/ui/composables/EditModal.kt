6827dcf732c3809592f6d66791bfe9c72922fe87
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-17 13:24:37 +0100

    Create rudimentary EditModal

--- app/src/main/java/com/cc221010/quickmaths/ui/composables/EditModal.kt
+++ app/src/main/java/com/cc221010/quickmaths/ui/composables/EditModal.kt
@@ -1,2 +1,14 @@
 package com.cc221010.quickmaths.ui.composables
 
+import androidx.compose.material.AlertDialog
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import com.cc221010.quickmaths.ui.mainViewModel
+
+@Composable
+fun editModal(mainViewModel:mainViewModel) {
+    val state = mainViewModel.mainViewState.collectAsState();
+    if (state.value.editModalOpen) {
+        AlertDialog(onDismissRequest = { mainViewModel.closeEditModal(false) }, buttons = { /*TODO*/ })
+    }
+}
