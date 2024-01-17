0f62fb0588cf54c77085eb81c7d16bc9379722c3
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-17 10:00:19 +0100

    Added Converters to Database

--- app/src/main/java/com/cc221010/quickmaths/data/scoreDatabase.kt
+++ app/src/main/java/com/cc221010/quickmaths/data/scoreDatabase.kt
@@ -2,9 +2,11 @@ package com.cc221010.quickmaths.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.cc221010.quickmaths.data.models.score
 
 @Database(entities = [score::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class scoreDatabase:RoomDatabase() {
     abstract val dao:scoreDAO;
 }
