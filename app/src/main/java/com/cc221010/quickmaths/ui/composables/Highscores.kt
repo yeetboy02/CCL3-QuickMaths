60edd828ed377bac13177964761abd28843319d2
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-17 13:36:25 +0100

    add EditModal to App

--- app/src/main/java/com/cc221010/quickmaths/ui/composables/Highscores.kt
+++ app/src/main/java/com/cc221010/quickmaths/ui/composables/Highscores.kt
@@ -2,12 +2,14 @@ package com.cc221010.quickmaths.ui.composables
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import com.cc221010.quickmaths.data.dateFormat
 import com.cc221010.quickmaths.ui.mainViewModel
 
@@ -15,11 +17,13 @@ import com.cc221010.quickmaths.ui.mainViewModel
 @Composable
 fun Highscores(mainViewModel:mainViewModel) {
     val state = mainViewModel.mainViewState.collectAsState();
-    var index:Int = 1;
 
     LazyColumn() {
+        var index:Int = 1;
         items(state.value.scores) {
-            Row() {
+            Row(
+                modifier = Modifier.clickable{ mainViewModel.openEditModal(it.id) }
+            ) {
                 Text(text = index.toString());
                 Text(text = it.name);
                 Text(text = it.points.toString());
@@ -28,4 +32,6 @@ fun Highscores(mainViewModel:mainViewModel) {
             index++;
         }
     }
+
+    editModal(mainViewModel);
 }
