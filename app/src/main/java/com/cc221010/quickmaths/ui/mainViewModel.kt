b03defe5ff5b7eda14853ce902d54d4fac2e2704
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-17 10:10:12 +0100

    Create Insert function at viewModel

--- app/src/main/java/com/cc221010/quickmaths/ui/mainViewModel.kt
+++ app/src/main/java/com/cc221010/quickmaths/ui/mainViewModel.kt
@@ -1,17 +1,27 @@
 package com.cc221010.quickmaths.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cc221010.quickmaths.data.models.score
+import com.cc221010.quickmaths.data.scoreDAO
 import com.cc221010.quickmaths.ui.composables.Screen
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class mainViewModel(): ViewModel() {
+class mainViewModel(private val dao: scoreDAO): ViewModel() {
     private val _mainViewState = MutableStateFlow(mainViewState());
     val mainViewState: StateFlow<mainViewState> = _mainViewState.asStateFlow();
 
     fun selectScreen(screen: Screen){
         _mainViewState.update { it.copy(selectedScreen = screen) }
+    }
+
+    fun addScore(score: score) {
+        viewModelScope.launch {
+            dao.insertScore(score);
+        }
     }
 }
