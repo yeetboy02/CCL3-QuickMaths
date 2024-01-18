5cd03daea3fa507fc5e21e931cebbcb3fa20ba7d
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-18 10:40:12 +0100

    Added GameViewState to GameViewModel

--- app/src/main/java/com/cc221010/quickmaths/ui/gameViewModel.kt
+++ app/src/main/java/com/cc221010/quickmaths/ui/gameViewModel.kt
@@ -1,4 +1,11 @@
 package com.cc221010.quickmaths.ui
 
-class gameViewModel {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class gameViewModel:ViewModel() {
+    private val _gameViewState = MutableStateFlow(gameViewState());
+    val gameViewState: StateFlow<gameViewState> = _gameViewState.asStateFlow();
 }
