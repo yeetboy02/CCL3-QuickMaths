fe611a48df9c423f0c0a533b20783a7cdbc3eb88
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-01-18 10:34:42 +0100

    Created Sequence Class

--- app/src/main/java/com/cc221010/quickmaths/helpers/Sequence.kt
+++ app/src/main/java/com/cc221010/quickmaths/helpers/Sequence.kt
@@ -1,4 +1,72 @@
 package com.cc221010.quickmaths.helpers
 
-class Sequence {
+@OptIn(ExperimentalStdlibApi::class)
+class Sequence(
+    override var numbers:List<Int> = emptyList(),
+    override val operators:List<Int> = (0..getRandInt(0, 2)).map { getRandInt(0, 2) },
+    var operatorNumbers:List<Int> = emptyList(),
+    override var length:Int = 0,
+    override var result:Int = 0,
+    override var asString:String = "",
+):Question() {
+    init {
+        var output:Int = 0;
+        if (operators.size * 2 > 4) {
+            output = getRandInt(4, operators.size * 2);
+        }
+        else {
+            output = getRandInt(operators.size * 2, 4);
+        }
+        length = output;
+    }
+
+    init {
+        var output:List<Int> = emptyList();
+        for (i in 0..<operators.size) {
+            if (operators[i] == 2) {
+                output += getRandInt(2, 4)
+            }
+            else {
+                output += getRandInt(1, 20);
+            }
+        }
+        operatorNumbers = output;
+    }
+
+    init {
+        var output:List<Int> = listOf(getRandInt(0, 100));
+
+
+        for (i in 0.. length) {
+            if (i == length) {
+                result = when (operators[i % operators.size]) {
+                    0 -> output[i] + operatorNumbers[i % operators.size];
+                    1 -> output[i] - operatorNumbers[i % operators.size];
+                    2 -> output[i] * operatorNumbers[i % operators.size];
+                    else -> output[i];
+                }
+                break;
+            }
+            output += when (operators[i % operators.size]) {
+                0 -> output[i] + operatorNumbers[i % operators.size];
+                1 -> output[i] - operatorNumbers[i % operators.size];
+                2 -> output[i] * operatorNumbers[i % operators.size];
+                else -> output[i];
+            }
+        }
+        numbers = output;
+    }
+
+    init {
+        var firstNumber:Int = numbers[0];
+        var output:String = "$firstNumber";
+
+        for (i in 1..<numbers.size) {
+            val currNumber:Int = numbers[i];
+            output = "$output ... $currNumber";
+        }
+
+        output += " ... ___";
+        asString = output;
+    }
 }
